9446c598d9392288e3703b9c0b23f3d0cf59c504
hawkw <hawk.weisman@gmail.com>
2014-04-15 11:57:23 -0400

    Add an (empty) test file

--- test/test.py
+++ test/test.py
@@ -1,2 +1,16 @@
 import sublime
 from unittest import TestCase
+
+version = sublime.version()
+
+class TestDiffListener(TestCase):
+
+    def setUp(self):
+        self.view = sublime.active_window().new_file()
+
+    def tearDown(self):
+        if self.view:
+            self.view.set_scratch(True)
+            self.view.window().run_command("close_file")
+
+    # put actual tests here
