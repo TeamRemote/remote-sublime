f4cdc92478fdcac379237e687275a27da4767ae1
hawkw <hawk.weisman@gmail.com>
2014-04-15 14:11:41 -0400

    restructure and add docs

--- remote.py
+++ remote.py
@@ -1,8 +1,7 @@
 import sublime, sublime_plugin
 
 def diff(old, new):
-    """Uses Operational Transformation to diff the new view 
-    against the old view."""
+    """Uses Operational Transformation to diff the new view against the old view."""
     # insert OT here
 
 class Session:
@@ -11,16 +10,16 @@ class Session:
         self.last_buffer = None
 
     def send_deltas(self, new_buffer):
-        """Sends deltas to the server over the current connection 
-        and sets the passed buffer as this view's buffer."""
+        """Sends deltas to the server over the current connection and sets the 
+        passed buffer as this view's buffer."""
         diff = diff(self.last_buffer, new_buffer)
         # TODO: insert code to actually send deltas here.
         self.last_buffer = new_buffer
 
 
 class DiffListener(sublime_plugin.EventListener):
-    """Listens for modifications to the view and gets the diffs
-    using Operational Transformation"""
+    """Listens for modifications to the view and gets the diffs using 
+    Operational Transformation"""
 
     def __init___(self):
         # watched_views is a dict of which currently open views are bound to 
@@ -30,7 +29,8 @@ class DiffListener(sublime_plugin.EventListener):
         self.watched_views = {}
 
     def on_modified_async(self, view):
-        """Listens for modifications to ."""
+        """Listens for modifications to views which are part of a currently
+        active remote session."""
         if view in watched_views.keys():
             # get the body text of the whole buffer
             buff = view.substr(sublime.Region(0, view.size()))
@@ -48,8 +48,8 @@ class StartSessionCommand(sublime_plugin.TextCommand):
         def run(self):     
             # this will have to connect to the remote server (getting the address
             # from the settings file), wait for the server to generate the session,
-            # and tell the user the access token. it'll then have to start watching the
-            # current view synchronizing
+            # and tell the user the access token. it'll then have to start watching
+            # the urrent view synchronizing
 
 class ConnectToSessionCommand(sublime_plugin.ApplicationCommand):
     """Command to connect to an external RemoteCollab session."""
